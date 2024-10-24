1e57a2929a24414890ec313c48b7d0e08474b399
SamAgravante <samson.rei95@gmail.com>
2024-10-25 03:59:36 +0800

    Small progress (Homepage)
    Added:
    - User Icon
    - Text Field

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,24 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Category from './Components/Category';
 import SkillOffering from './Components/SkillOffering';
 import './App.css'; 
+import Divider from '@mui/material/Divider';
+import UserIcon from './assets/images/UserIcon.png'
+import TextField from '@mui/material/TextField';
+
+// Usbon pani
+// Need himuon nga component ang home page para limpyo
 
 const App = () => {
     return (
         <Router>
             <div style={{ textAlign: 'center', margin: '20px' }}>
-            <div className="apptxt">WildSkills</div>
+                <div className="apptxt">
+                    <span style={{ alignSelf: 'start', display: 'flex'}}>WildSkills</span>
+                    <span style={{ alignSelf: 'end', display: 'flex' }}>
+                        <img src={UserIcon} alt="User Icon" style={{width: '65px', height:'65px'}}/>
+                    </span>
+                </div>
+
                 <nav>
                     <Link to="/categories" style={{ margin: '10px', textDecoration: 'none' }}>Categories</Link>
                     <Link to="/skill-offerings" style={{ margin: '10px', textDecoration: 'none' }}>Skill Offerings</Link>
