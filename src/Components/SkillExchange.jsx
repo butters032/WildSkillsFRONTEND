9beb551ec61518ba881f863a1862d63bba97ca72
Abel-O2 <samuel.abrenica01@gmail.com>
2024-10-28 23:56:43 +0800

    Updated Skill Exchange UI

--- src/Components/SkillExchange.jsx
+++ src/Components/SkillExchange.jsx
@@ -8,6 +8,8 @@ export default function SkillExchange() {
     const exctitleRef = useRef()
     const excstatRef = useRef()
     const excidRef = useRef()
+    const excscstartRef = useRef()
+    const excscendRef = useRef()
 
     const api = axios.create({
         baseURL: 'http://localhost:8080/api/wildSkills/skillExchange/',
@@ -31,7 +33,9 @@ export default function SkillExchange() {
     const editExchange = () => {
         api.put(`/putSkillExchangeDetails?id=${excidRef.current.value}`, {
             status: excstatRef.current.value,
-            title: exctitleRef.current.value,
+            title: nexctitleRef.current.value,
+            scheduledStart: excscstartRef.current.value,
+            scheduledEnd: excscendRef.current.value,
         })
         .then(() =>{
             exchangeReload();
@@ -41,11 +45,13 @@ export default function SkillExchange() {
         })
     }
 
-    const handleEditExchange = (id, status, title) =>{
+    const handleEditExchange = (id, status, title, scheduledStart, scheduledEnd) =>{
         if (excidRef.current){
             excidRef.current.value = id;
             excstatRef.current.value = status;
             nexctitleRef.current.value = title;
+            excscstartRef.current.value = scheduledStart;
+            excscendRef.current.value = scheduledEnd;
         }
     }
 
@@ -64,6 +70,8 @@ export default function SkillExchange() {
         api.post('/postSkillExchangeRecord', {
             status: 'Ongoing',
             title: exctitleRef.current.value,
+            scheduledStart: excscstartRef.current.value,
+            scheduledEnd: excscendRef.current.value,
         })
         .then((exc) =>{
             console.log(exc.data);
@@ -91,7 +99,7 @@ export default function SkillExchange() {
                             minWidth: 480, 
                             maxWidth:480, 
                             minHeight: 100, 
-                            maxHeight: 100, 
+                            maxHeight: 150, 
                             margin: "auto", 
                             marginTop:1,
                             borderRadius: 3,
@@ -99,6 +107,8 @@ export default function SkillExchange() {
                             padding:1 }}>
                             <Typography variant="h5">{exc.title}</Typography>
                             <Typography variant="body1" justifySelf={"left"}>Status: {exc.status}</Typography>
+                            <Typography variant="body2" justifySelf={"left"}>Scheduled Start: {new Date(exc.scheduledStart).toLocaleString("en-US", {dateStyle: "medium", timeStyle: "short"})}</Typography>
+                            <Typography variant="body2" justifySelf={"left"}>Scheduled End: {new Date(exc.scheduledEnd).toLocaleString("en-US", {dateStyle: "medium", timeStyle: "short"})}</Typography>
                             <Button variant="contained" color="success" onClick={() =>{handleEditExchange(exc.skillExchangeID, exc.status, exc.title)}}>Edit</Button>
                             <Button variant="contained" color="error" onClick={() => {deleteExchange(exc.skillExchangeID)}}>Delete</Button>
                         </Grid2>
@@ -116,10 +126,18 @@ export default function SkillExchange() {
                         <Stack direction={"column"} spacing={1}>
                         <Typography variant="h6">Edit Exchange</Typography>
                             <input type="hidden" id="id" ref={excidRef}/>
-                            <label htmlFor="estatus">Update Status</label>
-                            <input type="text" id="estatus" ref={excstatRef} placeholder="Status"/>
-                            <label htmlFor="edescription">Update Title</label>
+                            <label htmlFor="estatus">Update Status:</label>
+                            <select id="estatus" ref={excstatRef} defaultValue="Ongoing">
+                                <option value={"Ongoing"}>Ongoing</option>
+                                <option value={"Cancelled"}>Cancelled</option>
+                                <option value={"Completed"}>Completed</option>
+                            </select>
+                            <label htmlFor="edescription">Update Title:</label>
                             <input type="text" id="edescription" ref={nexctitleRef} placeholder="Title"/>
+                            <label>Update Schedule Start:</label>
+                            <input type="datetime-local" ref={excscstartRef} placeholder="Schedule Start"/>
+                            <label>Update Schedule End:</label>
+                            <input type="datetime-local" ref={excscendRef} placeholder="Schedule End"/>
                             <Button variant="contained" color="success" onClick={editExchange}>Edit Exchange</Button>
                         </Stack>
                     </Stack>
